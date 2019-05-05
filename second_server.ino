1f6f53c665d8fbea7dda2d1763417685539f15ce
Bruss Wainaina <kihoropaul@gmail.com>
2019-05-05 05:31:38 +0400

    Add files
    First commit

---
+++ second_server.ino
@@ -0,0 +1,51 @@
+/*  Accesspoint - station communication without router
+ *  see: https://github.com/esp8266/Arduino/blob/master/doc/esp8266wifi/soft-access-point-class.rst
+ *       https://github.com/esp8266/Arduino/blob/master/doc/esp8266wifi/soft-access-point-examples.rst
+ *       https://github.com/esp8266/Arduino/issues/504
+ *  Works with: station_bare_01.ino
+ */ 
+
+
+#include <ESP8266WiFi.h>
+
+char ssid[] = ".";           
+char pass[] = "jamii2yote";  
+
+WiFiServer server(9999);
+IPAddress IP(192,168,4,15);
+IPAddress mask = (255, 255, 255, 0);
+
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_AP_STA);
+
+  Serial.print("Connecting to: "); Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  WiFi.softAP("ESPServer", "espserver");
+  WiFi.softAPConfig(IP, IP, mask);
+  server.begin();
+  Serial.println();
+  Serial.println("Server started.");
+  Serial.print("IP: ");     Serial.println(WiFi.softAPIP());
+  Serial.print("MAC:");     Serial.println(WiFi.softAPmacAddress());
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+    }
+  String request = client.readStringUntil('\r');
+  Serial.println("********************************");
+  Serial.println("From the station: " + request);
+  client.flush();
+  Serial.print("Byte sent to the station: ");
+  Serial.println(client.println(request + "ca" + "\r"));
+  
+}
